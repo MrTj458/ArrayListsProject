079759b264cdd2b2ff9badea78dfaf5c6dbe6c06
MrTj458 <thod0127@740s-pl-122.local>
2015-10-07 13:31:31 -0600

    Added ListView Class

--- src/lists/contoller/ArrayListController.java
+++ src/lists/contoller/ArrayListController.java
@@ -1,15 +1,23 @@
 package lists.contoller;
 
+import java.util.ArrayList;
+import lists.view.ListView;
+
 public class ArrayListController
 {
-
+	private ArrayList<String> firstWords;
+	private ListView listDisplay;
+	
 	public ArrayListController()
 	{
-		
+		firstWords = new ArrayList<String>();
+		listDisplay = new ListView();
 	}
 	
 	public void start()
-	{
+	{	
+		firstWords.add("Hello Lists!");
 		
+		System.out.println(firstWords);
 	}
 }
