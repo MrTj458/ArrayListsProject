47e5f019722ea6191f97093713aaa1536858225b
MrTj458 <thod0127@740s-pl-122.local>
2015-10-09 13:53:42 -0600

    Added show other loop and added strings to firstWords array.

--- src/lists/contoller/ArrayListController.java
+++ src/lists/contoller/ArrayListController.java
@@ -16,15 +16,32 @@ public class ArrayListController
 	
 	public void start()
 	{	
-		firstWords.add("Hello Lists!");
+		firstWords.add("jlksjdfqiwuernmvncxzbxcv");
+		firstWords.add("bababababababababababababa");
+		firstWords.add(1, "CRYING INSANELY");
 		displayList();
+		firstWords.set(2, "mamamamamamamamamamamama");
+		displayList();
+		firstWords.remove(0);
+		displayList();
+		showOtherLoop();
 	}
 	
 	private void displayList()
 	{
+		popupDisplay.showList("The size of the list is: " + firstWords.size());
 		for(int spot = 0; spot < firstWords.size(); spot++)
 		{
 			popupDisplay.showList("The contents at spot " + spot + " are \"" + firstWords.get(spot) + "\"");
+			firstWords.remove(0);
+		}
+	}
+	
+	private void showOtherLoop()
+	{
+		for(String words : firstWords)
+		{
+			popupDisplay.showList(words);
 		}
 	}
 }
